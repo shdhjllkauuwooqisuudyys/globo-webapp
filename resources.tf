a4a146d2d653772ca29a22e6f3da11b998989b87
shdhjllkauuwooqisuudyys <121674623+shdhjllkauuwooqisuudyys@users.noreply.github.com>
2025-08-18 14:50:54 +1000

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -26,45 +26,45 @@ locals {
 ##################################################################################
 
 #resource "aws_instance" "main" {
-#  count         = length(var.public_subnets)
-#  ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-#  instance_type = var.instance_type
-#  subnet_id     = var.public_subnets[count.index]
-#  vpc_security_group_ids = [
-#    aws_security_group.webapp_http_inbound_sg.id,
-#    aws_security_group.webapp_ssh_inbound_sg.id,
-#    aws_security_group.webapp_outbound_sg.id,
-#  ]
-#
-#  key_name = module.ssh_keys.key_pair_name
-#
-#  tags = merge(local.common_tags, {
-#    "Name" = "${local.name_prefix}-webapp-${count.index}"
-#  })
-#
-#  # Provisioner Stuff
-#  connection {
-#    type        = "ssh"
-#    user        = "ec2-user"
-#    port        = "22"
-#    host        = self.public_ip
-#    private_key = module.ssh_keys.private_key_openssh
-#  }
-#
-#  provisioner "file" {
-#    source      = "./templates/userdata.sh"
-#    destination = "/home/ec2-user/userdata.sh"
-#  }
-#
-#  provisioner "remote-exec" {
-#    inline = [
-#      "chmod +x /home/ec2-user/userdata.sh",
-#      "sh /home/ec2-user/userdata.sh",
-#    ]
-#    on_failure = continue
-#  }
-#
-#}
+  count         = length(var.public_subnets)
+  ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  instance_type = var.instance_type
+  subnet_id     = var.public_subnets[count.index]
+  vpc_security_group_ids = [
+    aws_security_group.webapp_http_inbound_sg.id,
+    aws_security_group.webapp_ssh_inbound_sg.id,
+    aws_security_group.webapp_outbound_sg.id,
+  ]
+
+  key_name = module.ssh_keys.key_pair_name
+
+  tags = merge(local.common_tags, {
+    "Name" = "${local.name_prefix}-webapp-${count.index}"
+  })
+
+  # Provisioner Stuff
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    port        = "22"
+    host        = self.public_ip
+    private_key = module.ssh_keys.private_key_openssh
+  }
+
+  provisioner "file" {
+    source      = "./templates/userdata.sh"
+    destination = "/home/ec2-user/userdata.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ec2-user/userdata.sh",
+      "sh /home/ec2-user/userdata.sh",
+    ]
+    on_failure = continue
+  }
+
+}
 
 resource "null_resource" "webapp" {
 
